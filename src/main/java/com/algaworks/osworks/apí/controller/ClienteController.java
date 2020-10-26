2f264467411b41c744d777048c2b7e1493aa9636
Silas Caimi <silascaimi@live.com>
2020-10-26 17:02:17 -0300

    fazendo consulta de dados persistentes

--- src/main/java/com/algaworks/osworks/apí/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/apí/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.osworks.apí.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +13,12 @@ import com.algaworks.osworks.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		Cliente cliente2 = new Cliente();
 		
-		cliente1.setId(1L);
-		cliente1.setNome("Carlos");
-		cliente1.setEmail("carlos@xyz.com");
-		cliente1.setTelefone("99929999");
-		
-		cliente2.setId(2L);
-		cliente2.setNome("João");
-		cliente2.setEmail("joao@xyz.com");
-		cliente2.setTelefone("999884457");
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
