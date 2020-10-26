90168a52a73b93047d0bca6a32c02ca936070287
Silas Caimi <silascaimi@live.com>
2020-10-26 18:23:35 -0300

    implementa alteração de registro cliente no banco de dados

--- src/main/java/com/algaworks/osworks/apí/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/apí/controller/ClienteController.java
@@ -9,8 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,28 +22,42 @@ import com.algaworks.osworks.domain.repository.ClienteRepository;
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 	}
-	
+
 	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
-		
-		if(cliente.isPresent()) {
+
+		if (cliente.isPresent()) {
 			return ResponseEntity.ok(cliente.get());
 		}
 		return ResponseEntity.notFound().build();
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
+
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+
+		return ResponseEntity.ok(cliente);
+	}
+
 }
