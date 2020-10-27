1bb53559f3c92685ff5c078075b3fc8165b0224d
Silas Caimi <silascaimi@live.com>
2020-10-27 15:00:45 -0300

    implementa regra para tratar cadastro de cliente com email duplicado

--- src/main/java/com/algaworks/osworks/domain/service/CadastroClienteService.java
+++ src/main/java/com/algaworks/osworks/domain/service/CadastroClienteService.java
@@ -3,6 +3,7 @@ package com.algaworks.osworks.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.osworks.domain.exception.NegocioException;
 import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
 
@@ -13,6 +14,13 @@ public class CadastroClienteService {
 	private ClienteRepository clienteRepository;
 
 	public Cliente salvar(Cliente cliente) {
+		
+		Cliente clienteExistente = clienteRepository.findByEmail(cliente.getEmail());
+		
+		if (clienteExistente != null && !clienteExistente.equals(cliente)) {
+			throw new NegocioException("Já existe um cliente cadastrado com esse email");
+		}
+		
 		return clienteRepository.save(cliente);
 	}
 
