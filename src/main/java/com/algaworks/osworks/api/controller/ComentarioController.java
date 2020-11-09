4630867c361278f02ccf6fa73e329b45dd7ef04a
silascaimi <silascaimi@live.com>
2020-11-08 21:49:18 -0300

    implementando listar comentarios por ordemServicoId

--- src/main/java/com/algaworks/osworks/api/controller/ComentarioController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ComentarioController.java
@@ -1,10 +1,14 @@
 package com.algaworks.osworks.api.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.api.model.ComentarioInput;
 import com.algaworks.osworks.api.model.ComentarioModel;
+import com.algaworks.osworks.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.osworks.domain.model.Comentario;
+import com.algaworks.osworks.domain.model.OrdemServico;
+import com.algaworks.osworks.domain.repository.ComentarioRepository;
+import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 import com.algaworks.osworks.domain.service.GestaoOrdemServicoService;
 
 @RestController
@@ -23,9 +31,20 @@ public class ComentarioController {
 
 	@Autowired
 	private GestaoOrdemServicoService gestaoOrdemServico;
-
+	
+	@Autowired
+	private OrdemServicoRepository ordemServicoRepository;
+	
 	@Autowired
 	private ModelMapper modelMapper;
+	
+	@GetMapping
+	public List<ComentarioModel> listar(@PathVariable Long ordemServicoId) {
+		OrdemServico ordemServico = ordemServicoRepository.findById(ordemServicoId)
+				.orElseThrow(() -> new EntidadeNaoEncontradaException("Ordem de servico não encontrada"));
+		
+		return toColletionModel(ordemServico.getComentarios());
+	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
@@ -33,6 +52,11 @@ public class ComentarioController {
 			@Valid @RequestBody ComentarioInput comentarioInput) {
 		Comentario comentario = gestaoOrdemServico.adicionarComentario(ordemServicoId, comentarioInput.getDescricao());
 		return toModel(comentario);
+	}
+
+	private List<ComentarioModel> toColletionModel(List<Comentario> comentarios) {
+		return comentarios.stream().map(comentario -> toModel(comentario))
+				.collect(Collectors.toList());
 	}
 
 	private ComentarioModel toModel(Comentario comentario) {
