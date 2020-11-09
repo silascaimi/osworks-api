eb510e1efb9f13032a2aff77eb34cfdd9381a44f
silascaimi <silascaimi@live.com>
2020-11-08 21:32:24 -0300

    implementando uma EntidadeNaoEncontrada para tratamento de comentarioId inexistente em criar comentario

--- src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
+++ src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
@@ -5,6 +5,7 @@ import java.time.OffsetDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.osworks.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.osworks.domain.exception.NegocioException;
 import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.model.Comentario;
@@ -39,7 +40,7 @@ public class GestaoOrdemServicoService {
 
 	public Comentario adicionarComentario(Long ordemServicoId, String descricao) {
 		OrdemServico ordemServico = ordemServicoRepository.findById(ordemServicoId)
-				.orElseThrow(() -> new NegocioException("Ordem de serviço não encontrada"));
+				.orElseThrow(() -> new EntidadeNaoEncontradaException("Ordem de serviço não encontrada"));
 
 		Comentario comentario = new Comentario();
 		comentario.setDataEnvio(OffsetDateTime.now());
